ab5a8c6bdf158b1dedf8bbe2831da32f6bc3ae74
Oswaldo De Los Santos <ofdelossantosa@gmail.com>
2026-09-14 15:13:06 -0400

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+        if ([...s].sort().join("") === [...t].sort().join("")) {
+            return true;
+        }
+
+        return false;
+    
+    }
+}
